ee7d40deb152f582b94165cd3cb41eb8fb4a0339
BrianneBr <brianebresnahan@gmail.com>
2021-09-19 18:32:52 -0400

    Initial User Create Setup

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,6 +6,15 @@ const plugin: FastifyPluginAsync = async (fastify, opts) => {
             prop: new Date().toString()
         };
 	});
+
+    interface CreateUserRequest {
+        username: string;
+        password: string;
+    }
+
+    fastify.post<{Body: CreateUserRequest}>("/user/create", async (req) => {
+        return req.body;
+    });
 };
 
 export default plugin;
